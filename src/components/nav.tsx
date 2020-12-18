a57790f08e01e47060bca13d4d842eab1d31418e
Caelin Sutch <caelinsutch@gmail.com>
2020-12-17 22:22:41 -0800

    finished landing

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Flex, Box, Heading, Spacer, Link } from '@chakra-ui/react';
+import {graphql, useStaticQuery} from 'gatsby';
+import GatsbyImage from 'gatsby-image/index';
 
 const Nav: React.FC = () => {
+
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      file(relativePath: { eq: "logo-icon.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fixed(height: 50, width: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <Flex p="2rem">
-      <Box>
-        <Heading color="#545BF3" size="lg">SaaSHacks.fyi</Heading>
-      </Box>
+    <Flex p="2rem" alignItems="center">
+      <Flex alignItems="center">
+        <GatsbyImage  fixed={data.file.childImageSharp.fixed}  />
+        <Heading color="#545BF3" size="md">saashacks.fyi</Heading>
+      </Flex>
       <Spacer/>
       <Box>
-        <Link color="gray.500">
+        <Link color="gray.500" href="https://caelinsutch.substack.com/">
           See Past Articles
         </Link>
       </Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,52 +5,58 @@ import {Container, Heading, SimpleGrid, GridItem, Img, Box} from '@chakra-ui/rea
 import Layout from '../components/layout';
 import GatsbyImage from "gatsby-image"
 
-import {graphql} from 'gatsby';
-import styled from '@emotion/styled';
+import {graphql, useStaticQuery} from 'gatsby';
 import Nav from "../components/nav";
 
-const IndexPage = ({ data}) => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage: React.FC = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "home.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }`
+  )
+
+  return <Layout>
+    <SEO title="Home"/>
     <Nav/>
     <Container maxW="1080px">
-      <SimpleGrid columns={2}>
-        <GridItem pt="20vh">
+      <SimpleGrid columns={{base: 1, md: 2}}>
+        <GridItem pt="5vh" display={{base: 'block', md: 'none'}}>
+          <Box maxW="400px" mx="auto">
+            <GatsbyImage style={{marginLeft: 'auto'}} fluid={data.file.childImageSharp.fluid}/>
+          </Box>
+        </GridItem>
+        <GridItem pt={{base: '5rem', md: '20vh'}} textAlign={{base: 'center', md: 'left'}}>
           <Heading color="#545BF3" size="3xl" as="h1" lineHeight="150%">Growth Strategies for SaaS</Heading>
           <Heading as={"h3"}
                    mt={3}
                    fontWeight="regular"
                    color="gray.500"
-                   size="md">Get bite-sized tips and actionable strategies for growing SaaS startups. Includes a case study to see the strategy implemented in the real world.
+                   size="md">Get bite-sized tips and actionable strategies for growing SaaS startups. Includes a case
+            study to see the strategy implemented in the real world.
           </Heading>
-          <Box mt={3}>
-
+          <Box mt={3} mx={{base: 'auto', md: 0}}>
             <iframe src="https://caelinsutch.substack.com/embed" width="480" height="140"
-                    frameBorder="0" scrolling="no" style={{}}/>
+                    style={{marginLeft: 'auto', marginRight: 'auto'}}
+                    frameBorder="0" scrolling="no"/>
           </Box>
         </GridItem>
         <GridItem pt="15vh" display={{base: 'none', md: 'block'}}>
           <Box maxW="400px" pt="5rem" textAlign="left" ml="3rem">
-              <GatsbyImage style={{marginLeft: 'auto'}} fluid={data.file.childImageSharp.fluid}  />
+            <GatsbyImage style={{marginLeft: 'auto'}} fluid={data.file.childImageSharp.fluid}/>
           </Box>
         </GridItem>
       </SimpleGrid>
     </Container>
   </Layout>
-)
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "home.png" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
+}
 
 export default IndexPage
